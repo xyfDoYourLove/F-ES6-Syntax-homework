69ce8b2d981756037ed2c305989dac77c7b0db7e
DoYourLove <xyf15620168540@163.com>
2020-09-11 15:23:09 +0800

    [XiaoYiFan] feat : finish exercise

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = (input) => {
+    const {data, column} = input;
+    const result = [];
+    data.forEach((info) => {
+        let tmp = {}
+        column.forEach((value, index) => {
+            tmp[value.name] = info[index]
+        })
+        result.push(tmp);
+    })
+
+    return result;
+}
+
 export { parseData };
