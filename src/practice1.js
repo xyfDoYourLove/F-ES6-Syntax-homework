4ca3a752f4c98691379e2d647952090e570aff30
kevindongzg <dongkevin94@gmail.com>
2020-09-22 18:25:40 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,5 +1,7 @@
 const parseData = (input) => {
+    // TODO 解构可以优化到函数接受参数中
     const {data, column} = input;
+    // TODO 使用map做数组与数组的转化，不用声明新数组
     const result = [];
     data.forEach((info) => {
         let tmp = {}
